0092a7a128b5c2267018eb2670f31515eb2b5875
Ibrahdiallo <ibrahima.diallo@revature.net>
2022-03-24 07:52:06 -0700

    Spring project created

---
+++ src/main/java/com/revature/ibrahAzuredemo/IbrahAzureDemoApplication.java
@@ -0,0 +1,13 @@
+package com.revature.ibrahAzuredemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbrahAzureDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IbrahAzureDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/revature/ibrahAzuredemo/IbrahAzureDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.ibrahAzuredemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IbrahAzureDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
